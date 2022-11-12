310137321f83279850c98cc883f2f197470e9a63
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-11-12 16:28:40 -0300

    Add files via upload

---
+++ assets/preload/data/poprocky/script.lua
@@ -0,0 +1,20 @@
+
+function onBeatHit( ... )
+	if curBeat == 96 then
+
+        setProperty('i.visible',true)
+	end
+	if curBeat == 160 then
+
+        setProperty('i.visible',false)
+	end
+
+	if curBeat == 319 then
+
+        setProperty('i.visible',true)
+	end
+	if curBeat == 384 then
+
+        setProperty('i.visible',false)
+	end
+end
